a4c487cae22d9f7f4b13b9d3fb9c5678d13bccab
Llamba <marvelcalvero@gmailc.om>
2023-09-21 20:20:03 -0400

    Added newline at end of file

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,47 @@
-import java.util.Arrays;    // imports Arrays
-import java.util.Scanner;    // imports Scanner
-import java.io.FileNotFoundException;    // imports AFileNotFoundException
-import java.io.File;    // imports File
-import java.util.Random;    // imports Random
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        StringBuilder data = new StringBuilder();
-        Random rand = new Random();
 
-        try {
-            File myObj = new File("Words.txt"); // reading the file and adding all values to `data`
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                data.append(" " + myReader.nextLine());
-            }
 
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        System.out.println("Welcome to the tip calculator");
 
-        String parsedData[] = data.toString().split(" ");   // parsing the data to a array
-        String word = parsedData[rand.nextInt(1001)];   // getting a random word from the array
-        String letters[] = word.split("");  // making an array to contain each letter
 
-        System.out.println("Welcome to a word guessing game!");
-        System.out.println("Type anything to start");
-        scan.nextLine();
+        System.out.print("How many people are in your group: ");
+        int groupSize = scan.nextInt();
 
-        boolean win = false;    // adds boolean `win` and sets it to false
-        String guess;   // adds string guess
-        boolean correctLetter = false;  // adds boolean `correctLetter` and sets it to false
 
-        StringBuilder underscores = new StringBuilder();    // making an array, `found`, to contain all the found letters`
-        for (String element : letters) {
-            underscores.append("_ ");
-        }
-        String found[] = underscores.toString().split(" "); // adds string array `found` and sets it to be one underscore per letter in the word
+        System.out.print("What's the tip percentage? (0-100): ");
+        double tipDecimal = scan.nextInt() / 100.0;
 
-        while (!win) {
-            System.out.println("Please guess a letter or word");
-            guess = scan.nextLine();
 
-            if (guess.length() > 1) {
-                if (guess.equals(word)) {   // sets `win` to true when you win the game
-                    win = true;
-                } else {
-                    System.out.println("Not the word, try again");
-                }
-            } else {
-                for (int i = 0; i < letters.length; i++) {  // sets the underscores to letters when you guess them
-                    if (letters[i].equals(guess)) {
-                        found[i] = guess;
-                    }
-                }
-
-                boolean allLettersFound = Arrays.stream(found).noneMatch(s -> s.equals("_"));   // a check for when you guess all the letters
-                if (allLettersFound) {
-                    win = true;
-                }
-
-                System.out.println("Current progress: " + String.join(" ", found));
+        double cost = 0;
+        double itemCost = 0;
+        String items[] = {};
+        while (itemCost != -1) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+            itemCost = scan.nextDouble();
+            scan.nextLine();
+            if (itemCost != -1) {
+                System.out.print("Enter the item: ");
+                items = Arrays.copyOf(items, items.length + 1);
+                items[items.length - 1] = scan.nextLine();
+                cost += itemCost;
             }
         }
-
-        System.out.println("Congratulations! You win! The word is: " + word);
+        System.out.println("-------------------------------");
+        System.out.println("Total bill before tip: $" + cost);
+        System.out.println("Total percentage: " + ((int) (tipDecimal * 100)) + "%");
+        System.out.printf("Total tip: $%.2f %n", (cost * tipDecimal));
+        System.out.printf("Total bill with tip: $%.2f %n", (cost + cost * tipDecimal));
+        System.out.printf("Per person cost before tip: $%.2f %n", (cost / groupSize));
+        System.out.printf("Tip per person: $%.2f %n", ((cost * tipDecimal) / groupSize));
+        System.out.printf("Total cost per person: $%.2f %n", ((cost + cost * tipDecimal) / groupSize));
+        System.out.println("-------------------------------");
+        System.out.println("Items ordered:");
+        for (String item : items) {
+            System.out.println(item);
+        }
     }
 }
